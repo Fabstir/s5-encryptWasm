82615f5dfb7019cf61718323a7569192c7972377
parajbs-dev <parajbs@gmail.com>
2023-07-18 23:16:13 +0200

    smaller-fix2
    Changelog:
    - smaller-fix2

--- dist/cjs/src/index.d.ts
+++
@@ -1,4 +0,0 @@
-import __wbg_init, { encrypt_file_xchacha20, generate_key } from "../encrypt_file/pkg/encrypt_file";
-export { __wbg_init, encrypt_file_xchacha20, generate_key };
-export { chunkSizeAsPowerOf2, getEncryptedStreamReader, calculateB3hashFromFileEncrypt, removeKeyFromEncryptedCid, combineKeytoEncryptedCid, createEncryptedCid, encryptFile, } from "./utils/encryptWasm";
-//# sourceMappingURL=index.d.ts.map
